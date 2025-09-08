4474fa22d093bc4317f9ddb1be197417f07e0e71
M3096_csx <mithilesh_kumar@csx.com>
2025-09-08 14:21:59 +0530

    added token expired

--- myserver.js
+++ myserver.js
@@ -35,7 +35,12 @@ function authenticateToken(req, res, next) {
     if (!token) return res.status(401).json({ error: "Token missing" });
 
     jwt.verify(token, SECRET_KEY, (err, user) => {
-        if (err) return res.status(403).json({ error: "Invalid token" });
+        if (err) {
+             if (err.name === "TokenExpiredError") {
+                return res.status(401).json({ error: "Token expired" });
+            }
+            return res.status(403).json({ error: "Invalid token" });
+        }
         req.user = user;
         next();
     });
